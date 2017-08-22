6274445595c9e0b16d370739719ed538f67263b0
Douglas Sellers <doug@fitorb.com>
2017-08-22 00:02:40 -0700

    Pulling basic shelf data

--- bin/analyze_to_read_shelf.rb
+++ bin/analyze_to_read_shelf.rb
@@ -1,11 +1,11 @@
 require 'dotenv/load'
 require 'goodreads'
-puts ENV['GOODREADS_API_KEY']
+
 client = Goodreads::Client.new(api_key: ENV['GOODREADS_API_KEY'], api_secret: ENV['GOODREADS_SECRET_KEY'])
 page = 1
 more_pages = true
 while more_pages do
-  shelf = client.shelf(36167974, 'to-read', {page:page})
+  shelf = client.shelf(ENV['GOODREADS_USER_ID'], 'to-read', {page:page})
   books = shelf.books
   books.each do |book|
     puts "#{book['book']['isbn13']} / #{book['book']['title']}"
