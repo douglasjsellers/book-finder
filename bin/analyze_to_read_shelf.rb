72330911afa6a366f029a5bac3010582ccc4258c
Douglas Sellers <douglas.sellers@gmail.com>
2017-12-02 22:52:12 -0800

    Made it so that now it catches osme exceptions

--- bin/analyze_to_read_shelf.rb
+++ bin/analyze_to_read_shelf.rb
@@ -4,16 +4,21 @@ require 'goodreads'
 @chrome = Chrome.new
 
 def fetch_asin( asin )
-  puts asin
-  amazon = Amazon.new( @chrome )
-  book = amazon.find_book_by_asin( asin )
-  
-  puts "#{book.title},#{book.on_kindle_unlimited?},#{book.kindle_price}"
+  begin
+    amazon = Amazon.new( @chrome )
+    book = amazon.find_book_by_asin( asin )
+    
+    puts "#{book.title},#{book.on_kindle_unlimited?},#{book.kindle_price}"
+  rescue
+    puts "Bad ASIN: #{asin}"
+  end
 end
 
 client = Goodreads::Client.new(api_key: ENV['GOODREADS_API_KEY'], api_secret: ENV['GOODREADS_SECRET_KEY'])
 
 shelf = client.shelf(ENV['GOODREADS_USER_ID'], 'to-read')
-shelf.books.each do |book|
-  fetch_asin( book.to_h['book']['isbn'] ) if book.to_h['book']['isbn']
+(shelf.start .. shelf.end).each do |page|
+  client.shelf(ENV['GOODREADS_USER_ID'], 'to-read', {page: page} ).books.each do |book|
+    fetch_asin( book.to_h['book']['isbn'] ) if book.to_h['book']['isbn']
+  end
 end
