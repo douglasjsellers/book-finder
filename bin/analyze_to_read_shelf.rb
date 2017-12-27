ba1a086621181604ab59ba0c116ba93b99f93231
Douglas Sellers <douglas.sellers@gmail.com>
2017-12-26 23:57:04 -0800

    Made it handle goofy things like isbn13s

--- bin/analyze_to_read_shelf.rb
+++ bin/analyze_to_read_shelf.rb
@@ -3,7 +3,7 @@ require 'dotenv/load'
 require 'goodreads'
 @chrome = Chrome.new
 
-def fetch_asin( asin )
+def fetch_asin( asin, title )
   begin
     amazon = Amazon.new( @chrome )
 
@@ -11,11 +11,30 @@ def fetch_asin( asin )
     if( book )
       puts "#{asin}, #{book.title},#{book.on_kindle_unlimited?},#{book.kindle_price}"
     else
-      puts "#{asin}, No Kindle book"
+      puts "#{asin}, #{title}, No Kindle book"
     end
   rescue Exception => e
     puts "Bad ASIN: #{asin}: #{e.message}"
   end
+end
+
+def find_isbn( book, client )
+  to_return = nil
+  if book.to_h['book']['isbn']
+    to_return = book.to_h['book']['isbn']
+  else
+    #sometimes, for some strange reason, the isbn isn't included with a book that is returned via a self
+    book_with_details = client.book( book['book']['id'] )
+    if( book_with_details['asin'] && book_with_details['asin'].length > 0 )
+      to_return = book_with_details['asin']
+    elsif( book_with_details['isbn'] && book_with_details['isbn'].length > 0 )
+      to_return = book_with_details['isbn']
+    elsif( book_with_details['isbn13'] && book_with_details['isbn13'].length > 0 )
+      to_return = book_with_details['isbn13']
+    end
+  end
+  
+  to_return
 end
 
 client = Goodreads::Client.new(api_key: ENV['GOODREADS_API_KEY'], api_secret: ENV['GOODREADS_SECRET_KEY'])
@@ -23,11 +42,6 @@ client = Goodreads::Client.new(api_key: ENV['GOODREADS_API_KEY'], api_secret: EN
 shelf = client.shelf(ENV['GOODREADS_USER_ID'], 'to-read')
 (shelf.start .. shelf.end).each do |page|
   client.shelf(ENV['GOODREADS_USER_ID'], 'to-read', {page: page} ).books.each do |book|
-    if book.to_h['book']['isbn']    
-      fetch_asin( book.to_h['book']['isbn'] )
-    else
-      #sometimes, for some strange reason, the isbn isn't included with a book that is returned via a self
-      fetch_asin( client.book( book['book']['id'] ).to_h['asin'] )
-    end
+    fetch_asin( find_isbn( book, client ), book.to_h['book']['title'] )
   end
 end
