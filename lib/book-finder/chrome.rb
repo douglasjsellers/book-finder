9dbd747105031a575f6e2dfceeb4f79ca3148b18
Douglas Sellers <douglas.sellers@gmail.com>
2017-12-30 00:12:47 -0800

    Fixed deprecation warnings

--- lib/book-finder/chrome.rb
+++ lib/book-finder/chrome.rb
@@ -2,7 +2,10 @@ class Chrome
   def initialize
     @user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36"
     @options = Selenium::WebDriver::Chrome::Options.new(args: ['headless'])
-    @driver = Selenium::WebDriver.for(:chrome, options: @options, switches: %W[--user-agent=#{@user_agent} --window-size=1280,1696])
+    @options.add_argument( "user-agent=#{@user_agent}" )
+    @options.add_argument( "window-size=1280,1696" )
+    
+    @driver = Selenium::WebDriver.for(:chrome, options: @options )
   end
   def fetch_url( url )
     @driver.get(url)
