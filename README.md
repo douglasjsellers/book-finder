e42c34fba6f0280e3ff42877793cd4acff43f667
Douglas Sellers <douglas.sellers@gmail.com>
2018-01-01 00:00:54 -0800

    Updated the README

--- README.md
+++ README.md
@@ -1,1 +1,28 @@
-# I just want to know where to find books
+# Book Finder - Take your GoodReads to-read shelf and find out kindle and library availability
+The goal of this program is to help people spend as little as possible on books each year.  It does that by producing a CSV that takes all of the books on your goodreads to-read shelf and finds out what the kindle price is, if it's available on kindle unlimited, if it's available on any of the overdrive library cards that you have, how many copies are available at each library and how many people are waiting in each libraries queue.  This can then be used to facilitate a next book to read decision.
+
+## Setup
+After cloning this repo you will need to put a .env file in the root directory that is formatted like so:
+`GOODREADS_API_KEY='My API Key'
+GOODREADS_SECRET_KEY='MY SECRET KEY'`
+
+You can optain a api key and secret throught the goodreads api program here: https://www.goodreads.com/api/keys.  Once you have filled in this .env file you will then have to do a 
+`bundle install` 
+to get the appropriate dependencies installed.
+
+## Usage
+The most common usage of this tool is to analyze your to-read shelf for all information:
+
+`bundle exec ruby bin/analyze_a_to_read_shelf.rb <GOODREADS USER ID> <OVERDRIVE LIBRARY URLS>`
+
+The GoodReads user id can be found in the url of your to-read self.  For example:
+`https://www.goodreads.com/review/list/36167974?shelf=to-read`
+My GoodReads user id is 36167974.  
+
+The overdrive library urls can be found by going to the overdrive library search (https://www.overdrive.com/libraries), putting in your zip, clicking on your library and then clicking on the name of the library system that appears in the popup on the map.  For example, my zip code is 91411, my library is the "Sherman Oaks - Martin Pollard Library" and my library system is "Los Angeles Public Library".  Clicking this last link took me to "https://lapl.overdrive.com/" which is the overdrive url of my library.  So to analyze my shelf I would run:
+
+`bundle exec ruby bin/analyze_a_to_read_shelf.rb 36167974 https://lapl.overdrive.com/`
+
+If you happen to have multiple library cards you can run the same command with multiple library urls.  For example:
+`bundle exec ruby bin/analyze_your_to_read_shelf.rb https://hawaii.overdrive.com https://lapl.overdrive.com'
+Will run an analysis on your to-read shelf but look at two different overdrive libraries to see what the availability looks like.
---
+++ bin/analyze_a_to_read_shelf.rb
@@ -0,0 +1,80 @@
+require_relative '../lib/book-finder.rb'
+require 'dotenv/load'
+require 'goodreads'
+
+@chrome_overdrive = Chrome.new
+@chrome = Chrome.new
+if( ARGV.length == 0 )
+  puts "usage ruby analyze_a_to_read_shelf.rb USER_GOODREADS_ID OVERDRIVE_URL(s)"
+  puts "example ruby analyze_a_to_read_sheld.rb 36167974 https://hawaii.overdrive.com https://lapl.overdrive.com"
+else
+  @goodreads_user_id = ARGV.first  
+  @libraries = ARGV[1..ARGV.length]
+
+  def fetch_library_data( title, author )
+    results = @libraries.collect do |library|
+      OverDrive.new( library, @chrome_overdrive ).find_book( title, author )
+    end
+    results = results.compact
+    if( results.length > 0 )
+      results.join( "," )
+    elsif( @libraries.length > 0 )
+      "\"#{title}\",\"#{author}\", Not at any library"
+    end
+  end
+
+  def fetch_asin( asin, title, author )
+    begin
+      amazon = Amazon.new( @chrome )
+      book = amazon.find_kindle_book_by_asin( asin )
+      if( book )
+        puts "\"#{asin}\",\"#{title}\",#{book.on_kindle_unlimited?},#{book.kindle_price},#{fetch_library_data( title, author)}"
+      else
+        puts "\"#{asin}\",\"#{title}\",false,-1,#{fetch_library_data( title, author)}"
+      end
+    rescue Exception => e
+      @chrome_overdrive = Chrome.new
+      @chrome = Chrome.new
+      
+      if( e.message == "Net::ReadTimeout" )
+        fetch_asin( asin, title, author )
+      else
+        puts "Bad ASIN: #{asin}: #{e.message}"
+      end
+    end
+  end
+
+  def find_isbn( book, client )
+    to_return = nil
+    if book.to_h['book']['isbn']
+      to_return = book.to_h['book']['isbn']
+    else
+      #sometimes, for some strange reason, the isbn isn't included with a book that is returned via a self
+      book_with_details = client.book( book['book']['id'] )
+      if( book_with_details['asin'] && book_with_details['asin'].length > 0 )
+        to_return = book_with_details['asin']
+      elsif( book_with_details['isbn'] && book_with_details['isbn'].length > 0 )
+        to_return = book_with_details['isbn']
+      elsif( book_with_details['isbn13'] && book_with_details['isbn13'].length > 0 )
+        to_return = book_with_details['isbn13']
+      end
+    end
+    
+    to_return
+  end
+
+  if( @libraries.length > 0 )
+    puts "ASIN, Book Title, Kindle Unlimited, Kindle Price, Library Book Title, Library Book Author, Library URL, Books Available to Take Out, Number of People Waiting To Take Book Out"
+  else
+    puts "ASIN, Book Title, Kindle Unlimited, Kindle Price"
+  end
+
+  client = Goodreads::Client.new(api_key: ENV['GOODREADS_API_KEY'], api_secret: ENV['GOODREADS_SECRET_KEY'])
+
+  shelf = client.shelf(@goodreads_user_id, 'to-read')
+  (shelf.start .. shelf.end).each do |page|
+    client.shelf(@goodreads_user_id, 'to-read', {page: page} ).books.each do |book|
+      fetch_asin( find_isbn( book, client ), book.to_h['book']['title'], book.to_h['book']['authors']['author']['name'] )
+    end
+  end
+end
